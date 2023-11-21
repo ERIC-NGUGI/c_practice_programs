f32b1138bde30639c1eeb279e7a2e531bf1078f2
ERIC-NGUGI <ericmwenja@gmail.com>
2023-11-21 21:41:02 +0300

    practice makes perfect

--- practice18.c
+++ practice18.c
@@ -8,17 +8,6 @@
 
 int main(int argc, char *argv[])
 {
-	int count;
-
-	printf("This program was called with \"%s\" \n", argv[0]);
-	if (argc > 1)
-	{
-		for(count = 1; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-	}else {
-		printf("There is no output \n");
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ practice19.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+/*
+ * This is a practice program on conditional statements using if else and switch
+ *
+ * */
+
+/*int main(void)
+{
+	int x = 2;
+	 if (x == 1)
+	 {
+		printf("x is 1\n");
+	 }
+	 else if (x == 2)
+	 {
+		printf("x is 2\n");
+	 }
+	 else if (x == 3)
+	 {
+		printf("x is 3\n");
+	 }
+	 else
+	 {
+		printf("x is not 1 2 or 3\n");
+	 }
+
+	 return (0);
+}*/
+
+/*using switch*/
+
+int main(void)
+{
+	int x = 2;
+
+	switch(x);
+	case 1 : printf("x is 1");
+		 break;
+	case 2 : printf("x is 2");
+		 break;
+	case 3 : printf("x is 3");
+		 break;
+	
+}
