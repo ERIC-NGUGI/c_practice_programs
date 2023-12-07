097f49e45840388b10b1f93e17aceb0b7d994e93
ERIC-NGUGI <ericmwenja@gmail.com>
2023-12-07 02:52:25 +0000

    practice makes perfect

---
+++ practice25.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * An example of a three dimenional array
+ */
+
+int main(void)
+{
+	int arr[2][2][3] ={
+			{{1, 2, 3},{4, 5, 6}},
+			{{7, 8, 9},{10, 11, 12}}
+			};
+
+	int i, j, k;
+
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			for (k = 0; k < 3; k++)
+			{
+				printf("%d \n", arr[i][j][k]);
+			}
+		}
+	}
+	return (0);
+}
